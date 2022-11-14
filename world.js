9d5165b70a8158328cc68182db7e301cab23e127
Kyle Stultz <kylostultz@gmail.com>
2022-11-13 23:53:20 -0500

    Added initial starter files

--- world.js
+++ world.js
@@ -1,44 +1,49 @@
-window.onload= mystarter;
-	function mystarter(){
-	    let countrybtn = document.getElementById('lookup');
-	    let citybtn = document.getElementById('l2');
-	    let message = document.getElementById('result');
-	    console.log(countrybtn)
-	    countrybtn.addEventListener('click',function(element){
-	        element.preventDefault();
-	        var mi_form = document.getElementById("country").value;
-	        fetch("world.php"+"? country=" +mi_form)
-	        .then(response =>{
-	            if (response.ok){
-	                return response.text()
-	            } else{
-	                return Promise.reject("Something went wrong")
-	            }
-	        
-	        })
-	        .then(data => {
-	            message.innerHTML = data;
-	        })
-	        .catch(error => console.log('There was an error: ' + error));
-	    });
-	    citybtn.addEventListener('click',function(element){
-	        element.preventDefault();
-	        var mi_form = document.getElementById("country").value;
-	        var cities= document.getElementById("country").value;
-	        fetch("world.php"+"?country=" + mi_form + "&context=cities")
-	        .then(response =>{
-	            if (response.ok){
-	                return response.text()
-	            } else{
-	                return Promise.reject("Something went wrong")
-	            }
-	        
-	        })
-	        .then(data => {
-	            message.innerHTML = data;
-	        })
-	        .catch(error => console.log('There was an error: ' + error));
-	    });
-	
+window.onload = function() {
 
-	}
+    let btn = document.querySelector("#lookup");
+    let filler  = document.querySelector("#country");
+    let result = document.querySelector("#result");
+    let cbtn = document.querySelector("#clookup");
+
+    btn.addEventListener("click", function(e) {
+
+        e.preventDefault();
+        fetch("world.php?country="+filler.value)
+
+            .then(response => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    return Promise.reject("Error!")
+                }
+            })
+            .then(data => {
+                result.innerHTML = data;
+            })
+            .catch(error => console.log(error));
+
+    });
+
+    cbtn.addEventListener("click", function(e) {
+
+        e.preventDefault();
+        fetch("world.php?country="+filler.value + "&context=cities")
+
+            .then(response => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    return Promise.reject("Error!")
+                }
+            })
+            .then(data => {
+                result.innerHTML = data;
+            })
+            .catch(error => console.log(error));
+
+    });
+
+
+
+
+} 
