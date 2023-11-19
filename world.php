bf28f5e5e631059c3caca497b2c3e8ab0e4470ca
Kyle Stultz <kylostultz@gmail.com>
2023-11-19 17:59:29 -0500

    Exercise 5 complete

--- world.php
+++ world.php
@@ -1,17 +1,67 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$urlrequest = $_SERVER['REQUEST_URI'];
+$query = parse_url($urlrequest, PHP_URL_QUERY);
+parse_str($query, $param);
+
+$context = $param['context'];
+//echo $context;
+
+
+//echo $country ;
+$find_cities = $conn->query("SELECT * FROM countries JOIN cities ON countries.code = cities.country_code where countries.name LIKE '%$country%'");
+$find_country = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+$resultcountry = $find_country->fetchAll(PDO::FETCH_ASSOC);
+$resultcities = $find_cities->fetchAll(PDO::FETCH_ASSOC);
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php if($context == "cities"): ?>
+
+<table>
+  <tr>
+    <th> Name  </th>
+    <th> District </th>
+    <th> Population </th>
+  </tr>
+
+<?php foreach ($resultcities as $row): ?>
+<tr>
+  <td><?php echo $row['name']; ?></td>
+  <td><?php echo $row['district']; ?></td>
+  <td><?php echo $row['population']; ?></td>
+</tr>
+
 <?php endforeach; ?>
-</ul>
+</table>
+
+<?php else: ?>
+
+  <table>
+    <tr>
+      <th> Name  </th>
+      <th> Continent </th>
+      <th> Year of Independence  </th>
+      <th> Head of State </th>
+    </tr>
+
+  <?php foreach ($resultcountry as $row): ?>
+  <tr>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['continent']; ?></td>
+    <td><?php echo $row['independence_year']; ?></td>
+    <td><?php echo $row['head_of_state']; ?></td>
+  </tr>
+<?php endforeach; ?>
+<?php endif>
+
